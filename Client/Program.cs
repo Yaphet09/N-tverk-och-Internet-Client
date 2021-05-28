25db97246e0639989d37d6732b306d136f4f7d0c
Yaphet09 <55975096+Yaphet09@users.noreply.github.com>
2021-05-28 09:52:05 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            try
+            {
+                string address = "127.0.0.1";
+                int port = 8001;
+
+                //Ansluter till servern:
+                Console.WriteLine("Ansluter...");
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect(address, port);
+                Console.WriteLine("Ansluten!");
+
+                //Skriv in medelande att skicka:
+                Console.Write("Skriv in meddelande: ");
+                String message = Console.ReadLine();
+
+                //Konvertera meddelande till ASCII-bytes
+                Byte[] bMessage = System.Text.Encoding.ASCII.GetBytes(message);
+
+                //SKicka iväg meddelandet:
+                Console.WriteLine("Skickar...");
+                NetworkStream tcpStream = tcpClient.GetStream();
+                tcpStream.Write(bMessage, 0, bMessage.Length);
+
+                //tag emot meddelande från servern
+                byte[] bRead = new byte[256];
+                int bReadSize = tcpStream.Read(bRead, 0, bRead.Length);
+
+                //Konvertera meddelandet till ett string objekt och skriv ut
+                string read = "";
+                for (int i = 0; i < bReadSize; i++)
+                    read += Convert.ToChar(bRead[i]);
+                Console.WriteLine("Servern säger: " + read);
+
+                //Stäng anslutningen
+                tcpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
